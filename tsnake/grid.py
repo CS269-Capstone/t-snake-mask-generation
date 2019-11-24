df6c2b8580a9cf31baa780a9f25ed751935134bf
allen-n <allennikka@gmail.com>
2019-11-23 17:17:42 -0800

    Igorning image and pycache. Grid now reading images, computing intensities and force functions over image.

--- tsnake/grid.py
+++ tsnake/grid.py
@@ -4,16 +4,18 @@ Module containing implementations of the ACID technique.
 
 import numpy as np
 import snake as snake
+import cv2
+
 
 class GridCellEdge(object):
     """
     Represents one of the sides / cell-edges in the grid. 
     """
-    
+
     def __init__(self, coord1, coord2):
         """
         Represents one grid cell edge (one of three components of a TriangeCell).
-        
+
         Args:
         ==========================================
         (2-tuple) coord1, coord2:
@@ -22,12 +24,12 @@ class GridCellEdge(object):
           (to be compatible with numpy indexing) (can change this if inconvenient)
         ==========================================
         """
-        self._coord1 = coord1  # todo maybe argchecks 
+        self._coord1 = coord1  # todo maybe argchecks
         self._coord2 = coord2
-        
+
         # TODO: implement this data structure
         self.intersections = dict()
-        
+
     @property
     def endpoints(self):
         return {self._coord1, self._coord2}
@@ -40,7 +42,7 @@ class GridCellEdge(object):
             - Return None
         """
         raise NotImplementedError
-    
+
 
 class CellGrid(object):
     """
@@ -48,55 +50,61 @@ class CellGrid(object):
       - image meaning the blank space the T-snake is segmenting / infilling
       - assumes that each triangle-cell is a right triangle
         (for the Coxeter-Freudenthal triangulation) (see Fig 2 in the paper)
-        
+
       - assumes (for now) that the space we're segmenting / infilling is rectangular
-    
-    
+
+
     In the paper, Demetri mentions the 'Freudenthal triangulation' for 
     implementing the cell-grid:
      https://www.cs.bgu.ac.il/~projects/projects/carmelie/html/triang/fred_T.htm
-    
+
     Args:
     ==========================================
     (np.array) image:
-    * (n by m) matrix representing the gray-scale image.
+    * (n by m) matrix representing the color image.
 
     (float) scale:
     * float between 0 and 1 representing the number of pixels per cell, i.e. 1=1 vertex/pixel, .5 = 2 vertex per pixel, so on
-    
+
     ==========================================
     """
-    
+
     def __init__(self, image, scale=1.0):
         """
         @allen: Should we pass a snake to the board? should the board own the snake?
         TODO: implement Freudenthal triangulation
         https://www.cs.bgu.ac.il/~projects/projects/carmelie/html/triang/fred_T.htm
         """
-        assert isinstance(image, np.array)
-        assert len(image.shape) == 2
+        assert isinstance(image, np.ndarray)
+        assert len(image.shape) == 3 # height * width * color channels
         assert type(scale) == float
+
+        # Raw image
         self.image = image
-        self.m, self.n = image.shape
+        self.m, self.n , self.d = image.shape
+
+        # Image matrix after force and intensity function
+        self.image_force = None
+        self.image_intensity = None
+
+        # Simplex Grid Vars
         self.scale = scale
-        self.n  = None # TODO: Implement gen_simplex_grid
-        self.edges = dict() # Hash set containing [sorted((x1, y1), (x2,y2))]:edge, sort to resolve ambiguity
-        self.force_grid = None # same size as grid
-        self.intensity_grid = None # same size as grid
-        self.snakes = list() # All the snakes on this grid
-    
-        raise NotImplementedError
+        self.grid = None  # TODO: Implement gen_simplex_grid
+        # Hash set containing [sorted((x1, y1), (x2,y2))]:edge, sort to resolve ambiguity
+        self.edges = dict()
+        self.snakes = list()  # All the snakes on this grid
+        # print("Grid initialized with:\n\theight: {}\n\twidth: {}\n\tdepth: {}".format(self.m, self.n, self.d))
 
     def gen_simplex_grid(self):
         """
         Private method to generate simplex grid and edge map over image at given scale
         self.grid = np array of size (n/scale) * m/scale
-        
+
         * Verticies are on if positive, off if negative, and contain 
             bilinearly interpolated greyscale values according to surrouding pixels
-        
+
         * vertex position indicated by its x and y indicies
-        """   
+        """
         # for point in grid:
         #     for neighbor points:
         #         if sorted(point, neighbor) not in self.edges:
@@ -104,17 +112,34 @@ class CellGrid(object):
 
         raise NotImplementedError
 
-    def gen_force_grid(self):
+    def get_image_force(self, threshold):
         """
         generate force grid over simplex grid, will need to interpolate
         """
-        raise NotImplementedError
+        if self.image_force is  None:
+            intensity = self.get_image_intensity()
+            self.image_force = np.zeros(intensity.shape) - 1
+            self.image_force[intensity >= threshold] = 1
 
-    def gen_intensity_grid(self):
+        return self.image_force
+
+    def get_image_intensity(self):
         """
-        generate intensity grid over simpelx grid, will need to interpolate
+        Compute's intensity of self.image
+        
+        Args:
+        ========================
+        None
+        ========================
+        Return:
+        ========================
+        (np.array) intensities: 
+        * (self.image.shape[0] by self.image.shape[1]) array of of intensities (values of 0 to 255)
+        ========================
         """
-        raise NotImplementedError
+        if self.image_intensity is None:
+            self.image_intensity = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        return self.image_intensity
 
     def add_snake(self, new_snake):
         """
@@ -122,45 +147,37 @@ class CellGrid(object):
         """
         assert isinstance(new_snake, snake.Snake)
         self.snakes.append(new_snake)
-    
+
     def compute_intersections(self, snake):
         """
         Compute intersections between the grid and the snake in question
         """
         # Get snake nodes, see which grid verticies they're closest to,
-        # grab all the edges between the verticies they're closest to, 
+        # grab all the edges between the verticies they're closest to,
         # compute intersections
         # enqueue grid vertex inside the snake
 
-    
-    
-    
-    
-    
-    
-    
-    
+
 ### TODOS ###
 # 1. snake updates - Joe
 # 2. gan mask -> snake -> gan mask - Cole
 # 3. algo phase 1: grid intersections - Allen
 # 4. algo phase 2: turning nodes on / off, remove inactive points - Eric
-    
-    
-    
-    
-    
 
-    
-    
+
 if __name__ == '__main__':
-    pass
+    # Import testing
+    snake = snake.Node(1,1) 
+    edge = GridCellEdge(1, 1)
 
-
-
-
-
-
-
-
-
+    # Manual Testing import
+    img = cv2.imread("plane.png")
+    grid = CellGrid(img)
+    grey = grid.get_image_intensity()
+    force = grid.get_image_force(250)
+    # print(grey, np.max(grey), grey.shape)
+    print(force, np.max(force), force.shape)
+    cv2.imshow("image", img)
+    cv2.imshow("grey_image", grey)
+    cv2.imshow("force_image", force)
+    key = cv2.waitKey(0)
