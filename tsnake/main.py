d6712ed5033eab2c4a31f081a61899aa823be5a0
Cole Zimmerman <czimmer112@gmail.com>
2019-12-19 14:06:34 -0600

    Remove now-broken inpainting code ;(

--- tsnake/main.py
+++ tsnake/main.py
@@ -10,7 +10,7 @@ import numpy.linalg as la
 import tsnake.initialize as init
 import tsnake.utils as utils
 from tsnake.grid import Grid
-from jiahui.inpaint import inpaint as j_inpaint
+# from jiahui.inpaint import inpaint as j_inpaint
 
 
 class Main(object):
@@ -60,13 +60,14 @@ class Main(object):
         if which_mask == 'snake' and self.snake_mask is None:
             msg = 'T-snake has not been run yet - please call Main.run() first.'
             raise ValueError(msg)
-
-        if which_mask == 'user':
-            output = j_inpaint(self.color_image, self.user_mask)
-            self.user_output_image = output
-        else:
-            output = j_inpaint(self.color_image, self.snake_mask)
-            self.snake_output_image = output
+        
+        # inpainting no longer works :( 
+#         if which_mask == 'user':
+#             output = j_inpaint(self.color_image, self.user_mask)
+#             self.user_output_image = output
+#         else:
+#             output = j_inpaint(self.color_image, self.snake_mask)
+#             self.snake_output_image = output
 
         return output
 
@@ -100,7 +101,8 @@ class Main(object):
                     are_on += 1
                 mask[i, j] = 255 if grid[i, j].is_on else 0
 
-        print("Main found {} nodes to be on".format(are_on))
+        total_num_nodes = n * m
+        print("Main found [{}/{}] nodes to be on".format(are_on, total_num_nodes))
         return mask
 
     def run(self, max_iter=1000, grid_scale=1.0, tolerance=0.5, **snake_params):
